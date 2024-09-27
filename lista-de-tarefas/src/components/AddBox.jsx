ec69c1624d267326f616264a29688fc5ef05092e
Ricardo Henrique Silva Rodrigues <135380624+1RiKaS1@users.noreply.github.com>
2024-09-27 20:16:58 -0300

    Adicionando novas coisas

---
+++ lista-de-tarefas/src/components/AddBox.jsx
@@ -0,0 +1,12 @@
+//Biblioteca
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+//Css
+import "../css/AddBox.css"
+
+export default function AddBox(){
+    return(
+        <div className="addBox_background">
+            	<button><AddCircleIcon>add_circle</AddCircleIcon></button>
+        </div>
+    )
+}
--- lista-de-tarefas/src/components/BoxListBackGround.jsx
+++ lista-de-tarefas/src/components/BoxListBackGround.jsx
@@ -1,9 +1,13 @@
 import '../css/BoxListBackGround.css'
 import NoteBox from './NoteBox'
+import AddBox from './AddBox'
+
 export default function BoxListBackGround(){
     return(
-        <div className="boxList_backGround">    
+        <div className="boxList_backGround">   
+            <h1 className="titulo">Lista de Atividade</h1> 
             <NoteBox/>
+            <AddBox/>
         </div>
     )
 }
